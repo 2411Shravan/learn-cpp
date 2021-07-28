9e16f15a6f15d12d3b2fa1fc85fc111e90a0779e
Shravan .V <perfectparameter24@gmail.com>
2021-07-28 12:43:09 +0530

    Datatypes in cpp along with their memory bytes

---
+++ datatypes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    char c;
+    bool b;
+    long e;
+    float r;
+    long long f;
+
+
+    cout<<"size of int is :"<<sizeof(a)<<endl;
+    cout<<"size of float is :"<<sizeof(r)<<endl;
+    cout<<"size of long is :"<<sizeof(e)<<endl;
+    cout<<"size of char is :"<<sizeof(c)<<endl;
+    cout<<"size of bool is :"<<sizeof(b)<<endl;
+    cout<<"size of long long is :"<<sizeof(f)<<endl;
+    cout<<"size of short int is :"<<sizeof(short int)<<endl;
+    cout<<"size of long int is :"<<sizeof(long int)<<endl;
+
+}
