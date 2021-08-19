6248c181cf5e32f46a1c809fa6e80ba7069b64da
Shravan .V <perfectparameter24@gmail.com>
2021-08-19 20:36:27 +0530

    array problems-1 solving max till i^th element

---
+++ maxtillI.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+
+int main(){
+    int a;
+    cout<<"Enter the size of array"<<endl;
+    cin>>a;
+    int ar[a];
+    int mx=-111111;
+    for(int i=0;i<a;i++){
+        cout<<"Enter the "<<i+1<<" th element"<<endl;
+        cin>>ar[i];
+        }
+    int c=1;
+    for(int i=0;i<a;i++){
+        
+        mx=max(mx,ar[i]);
+    }
+
+
+    cout<<mx<<endl;
+    
+}
