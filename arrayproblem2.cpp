8774be56580276bc49d4b85f9d8c767bf639f413
Shravan .V <68394410+2411Shravan@users.noreply.github.com>
2021-08-22 20:19:20 +0530

    array problem
    https://github.blog/2020-12-15-token-authentication-requirements-for-git-operations/

---
+++ arrayproblem2.cpp
@@ -0,0 +1,43 @@
+// record breaking number finding problem asked in google kickstart
+
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main(){
+    int a;
+    cout<<"Enter the size the array : "<<endl;
+    cin >> a;
+    int arr[a+1];
+    arr[a]=-1;
+    cout<<"Enter the elements to the array : "<<endl;
+    for(int i=0;i<a;i++){
+        cin>>arr[i];
+    }
+    int count=0;
+    int maximumElement=-1;
+
+
+    cout<<"Record breaking numbers are "<<endl;
+    if(a==1){
+        cout<<1<<endl;
+    }
+    else{
+        for(int i=0;i<a;i++){
+            if(arr[i]>maximumElement && arr[i]>arr[i+1]){
+                count++;
+                cout<<arr[i]<<endl;
+            }
+            
+            maximumElement=max(maximumElement,arr[i]);
+            
+        }
+    }
+
+    cout<<"Total record breaking numbers are : "<<count<<endl;
+
+
+}
