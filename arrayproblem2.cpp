a1771ccd9195285e788cb0eb00aa1ad50c2fc7e4
Shravan .V <perfectparameter24@gmail.com>
2021-08-26 18:23:43 +0530

    changes in record breaking problem

--- arrayproblem2.cpp
+++ arrayproblem2.cpp
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-// record breaking number finding problem asked in google kickstart
 
-
-#include<iostream>
-#include<algorithm>
-
-using namespace std;
-
-
-int main(){
-    int a;
-    cout<<"Enter the size the array : "<<endl;
-    cin >> a;
-    int arr[a+1];
-    arr[a]=-1;
-    cout<<"Enter the elements to the array : "<<endl;
-    for(int i=0;i<a;i++){
-        cin>>arr[i];
-    }
-    int count=0;
-    int maximumElement=-1;
-
-
-    cout<<"Record breaking numbers are "<<endl;
-    if(a==1){
-        cout<<1<<endl;
-    }
-    else{
-        for(int i=0;i<a;i++){
-            if(arr[i]>maximumElement && arr[i]>arr[i+1]){
-                count++;
-                cout<<arr[i]<<endl;
-            }
-            
-            maximumElement=max(maximumElement,arr[i]);
-            
-        }
-    }
-
-    cout<<"Total record breaking numbers are : "<<count<<endl;
-
-
-=======
-// record breaking number finding problem asked in google kickstart
 
 
 #include<iostream>
@@ -83,6 +39,5 @@ int main(){
 
     cout<<"Total record breaking numbers are : "<<count<<endl;
 
-
->>>>>>> 8774be56580276bc49d4b85f9d8c767bf639f413
 }
+
