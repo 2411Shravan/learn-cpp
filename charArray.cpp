e2ac3f3dadec3e630c900ce711012360d752e7b9
Shravan .V <perfectparameter24@gmail.com>
2021-08-26 19:54:37 +0530

    array of characters illustrations

---
+++ charArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main(){
+    char c[100];
+    cin>>c;
+    int i=0;
+    while(c[i]!='\0'){
+        cout<<c[i]<<endl;
+        i++;
+    }
+}
