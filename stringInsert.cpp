2de90d9e50019d007bc417e375e84dcdb18ec2f6
Shravan .V <perfectparameter24@gmail.com>
2021-08-28 20:35:34 +0530

    string insert to a string in cpp

---
+++ stringInsert.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s1="Shravan";
+    cout<<s1<<endl;
+    s1.insert(7," is programming");
+    cout<<s1<<endl;
+}
