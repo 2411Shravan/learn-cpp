053e5fac827c47bfc62755ff484b05519a5fa3b2
Shravan .V <perfectparameter24@gmail.com>
2021-08-28 20:39:11 +0530

    iterate a string using for loop in cpp

---
+++ stringIterate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s1="Shravan is programming";
+    for(int i=0;i<s1.length();i++){
+        cout<<s1[i]<<endl;
+    }
+}
