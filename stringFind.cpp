b67f5e1b0f8b77d9a8fe1103ce4affb8dd16c493
Shravan .V <perfectparameter24@gmail.com>
2021-08-28 20:33:50 +0530

    string find in cpp

---
+++ stringFind.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s1="Shravan";
+    
+    cout<<s1<<endl;
+    int loc=s1.find("van");
+    cout<<loc<<endl;
+}
