5a0b5ea5562d7f0aa583ebeb79a7c552f82d5682
Shravan .V <perfectparameter24@gmail.com>
2021-08-28 20:42:17 +0530

    substring in strings in cpp

---
+++ stringSubstring.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s1="Shravan is programming";
+    string s=s1.substr(3,9);
+   cout<<s1<<endl;
+   cout<<s<<endl;
+}
