2917d652a8407551a3680ae295f0184b60d76ef5
Shravan .V <perfectparameter24@gmail.com>
2021-09-13 18:38:32 +0530

    string replace using recursion in cpp

---
+++ recursionReplaceString.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+
+
+
+using namespace std;
+
+void replaceStr(string nm){
+    if(nm.length()==0){
+        return;
+    }
+    if(nm[0]=='p' && nm[1]=='i'){
+        cout<<"3.14 ";
+        replaceStr(nm.substr(2));
+    }
+    else{
+        cout<<nm[0];
+        replaceStr(nm.substr(1));
+    }
+}
+
+int main(){
+    string name="pipippppipipipipipppip";
+    replaceStr(name);
+}
