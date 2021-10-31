cae8165c3f2699899d3cf6dec846161da99232ae
Shravan .V <perfectparameter24@gmail.com>
2021-10-31 17:05:55 +0530

    traversing data present in  vectors in cpp

--- vector.cpp
+++ vector.cpp
@@ -8,12 +8,17 @@ int main(){
     //declaring vectors
 
     vector<int> v;
+    //adding data to vector
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
     v.push_back(4);
     v.push_back(5);
+    //traversing vector data in cpp
 
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
 
-
+    return 0;
 }
