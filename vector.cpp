746024ad33f223d8247aefffdcef5d37bc7975d7
Shravan .V <perfectparameter24@gmail.com>
2021-10-31 17:03:52 +0530

    all about vectors in cpp

---
+++ vector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+    //declaring vectors
+
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+
+
+}
