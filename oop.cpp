f954e0f7c1521a2aaf5dbd0efca5abf60c8c9ac6
Shravan .V <perfectparameter24@gmail.com>
2021-11-03 10:10:30 +0530

    started oop in cpp

---
+++ oop.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class student{
+    public:
+    string name;
+    int age;
+    int number;
+};
+
+
+
+int main(){
+    student a;
+    a.age = 25;
+    a.name="shravan";
+    a.number=63;
+    cout<<a.age<<endl;
+    cout<<a.name<<endl;
+    cout<<a.number<<endl;
+    return 0;
+}
+
